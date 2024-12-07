913d58c816cfcceff371c6f07da88def62433e77
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-07 02:14:21 +0100

    feat: create a Sidebar component

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import './Sidebar.scss';
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+      <div className='sidebar__container'>Sidebar</div>
+    </aside>
+  );
+};
+
+export default Sidebar;
