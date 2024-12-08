142b75d1dd7963729617988d6b1526432c25efe7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-08 20:53:10 +0100

    feat: add login form html markup

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,9 +1,66 @@
+import { Link } from 'react-router-dom';
+
 import './Login.scss';
 
 const Login = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='login'>
-      <div className='login__container'>Login</div>
+      <div className='login__container'>
+        <div className='login__wrapper'>
+          <h1 className='login__heading'>Welcome back</h1>
+          <p className='login__text'>
+            Welcome back! Please enter your details.
+          </p>
+
+          <form className='form' onSubmit={handleSubmit}>
+            <div className='form__group'>
+              <label htmlFor='identifier' className='form__label'>
+                Email/username
+              </label>
+              <input
+                id='identifier'
+                type='text'
+                name='identifier'
+                placeholder='Email/username'
+                className='form__input'
+              />
+            </div>
+
+            <div className='form__group'>
+              <label htmlFor='password' className='form__label'>
+                Password
+              </label>
+              <input
+                id='password'
+                type='password'
+                name='password'
+                placeholder='Password'
+                className='form__input'
+              />
+            </div>
+
+            <div className='form__forgot'>
+              <Link to='/forgot-password'>Forgot password</Link>
+            </div>
+
+            <div className='form__buttons'>
+              <button type='submit'>Login</button>
+              <button type='button'>Sign in with Google</button>
+            </div>
+          </form>
+
+          <div className=''>
+            <span>
+              Don't have an account?{' '}
+              <Link to='/register'>Sign up for free</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
