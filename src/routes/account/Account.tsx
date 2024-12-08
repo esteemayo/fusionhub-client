fd79ac1e86ce70a49efbf75ca250b9d612ba0f29
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-08 03:21:24 +0100

    feat: create Account page component

---
+++ src/routes/account/Account.tsx
@@ -0,0 +1,11 @@
+import './Account.scss';
+
+const Account = () => {
+  return (
+    <div className='account'>
+      <div className='account__container'>Account</div>
+    </div>
+  );
+};
+
+export default Account;
