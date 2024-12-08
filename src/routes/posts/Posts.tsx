b090c37a63a285eca4ea0219169b670abdbdcc1d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-08 02:57:17 +0100

    feat: create Posts page component

---
+++ src/routes/posts/Posts.tsx
@@ -0,0 +1,11 @@
+import './Posts.scss';
+
+const Posts = () => {
+  return (
+    <div className='posts'>
+      <div className='posts__container'>Posts</div>
+    </div>
+  );
+};
+
+export default Posts;
