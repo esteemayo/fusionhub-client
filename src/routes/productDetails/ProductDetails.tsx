60fa01fd02db7f7dcc6a24191e35fd6807c12860
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-08 03:11:57 +0100

    feat: create ProductDetails page component

---
+++ src/routes/productDetails/ProductDetails.tsx
@@ -0,0 +1,11 @@
+import './ProductDetails.scss';
+
+const ProductDetails = () => {
+  return (
+    <div className='productDetails'>
+      <div className='productDetails__container'>ProductDetails</div>
+    </div>
+  );
+};
+
+export default ProductDetails;
