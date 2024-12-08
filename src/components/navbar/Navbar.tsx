52cdb3b6b240d2e9e2287d27d2ea86b9b9bd03f3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-08 03:36:35 +0100

    fix: modify

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar__container'>
-        <div className='navbar__container--logo'>Logo</div>
+        <div className='navbar__container--logo'>
+          <Link to='/'>Logo</Link>
+        </div>
         <ul className='navbar__container--links'>
           <li className='navbar__container--links-item'>
             <Link to='/'>Home</Link>
