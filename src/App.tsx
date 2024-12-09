2bae7edbcf9219d326992a53eeddcd7142c51cd1
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 01:18:53 +0100

    fix: modify

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './utils/router';
 
-import './global.scss';
-
 function App() {
   return <RouterProvider router={router} />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
+import './global.scss';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
