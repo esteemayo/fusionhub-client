d961e52cb9dc74c56533e64daea249aaffe8ce58
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 01:25:51 +0100

    feat: add styling

--- src/components/errorMessage/ErrorMessage.tsx
+++ src/components/errorMessage/ErrorMessage.tsx
@@ -1,7 +1,11 @@
 import './ErrorMessage.scss';
 
 const ErrorMessage = ({ message }: { message?: string }) => {
-  return <span>{message}</span>;
+  return (
+    <div className='errorMessage'>
+      <span>{message}</span>
+    </div>
+  );
 };
 
 export default ErrorMessage;
