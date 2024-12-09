1252c6f89632286f75af64aa5ac762ae718c35e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 01:48:53 +0100

    feat: add html markup

--- src/routes/forgotPassword/ForgotPassword.tsx
+++ src/routes/forgotPassword/ForgotPassword.tsx
@@ -1,10 +1,17 @@
 import './ForgotPassword.scss';
 
 const ForgotPassword = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='forgotPassword'>
       <div className='forgotPassword__container'>
-        <div className='forgotPassword__wrapper'>ForgotPassword</div>
+        <div className='forgotPassword__wrapper'>
+          <h1 className='forgotPassword__heading'>Forgot password</h1>
+          <form onSubmit={handleSubmit}></form>
+        </div>
       </div>
     </div>
   );
