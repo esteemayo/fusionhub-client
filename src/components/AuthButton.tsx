37a84badf0c497de9666115f6b7c5f3d9beb2a7f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 17:48:19 +0100

    fix: modify

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,10 +1,10 @@
 import Button from './button/Button';
 import GoogleButton from './GoogleButton';
 
-const AuthButton = () => {
+const AuthButton = ({ label }: { label: string }) => {
   return (
     <div className='auth__button'>
-      <Button type='submit' label='Register' className='primary' />
+      <Button type='submit' label={label} className='primary' />
       <GoogleButton />
     </div>
   );
