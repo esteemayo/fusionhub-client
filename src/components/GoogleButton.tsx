41916205862db8b79a3bc03f80bba294eee28298
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 15:42:30 +0100

    feat: create GoogleButton component

---
+++ src/components/GoogleButton.tsx
@@ -0,0 +1,9 @@
+import Button from './button/Button';
+
+const GoogleButton = () => {
+  return (
+    <Button img='/google.png' label='Sign in with Google' className='outline' />
+  );
+};
+
+export default GoogleButton;
