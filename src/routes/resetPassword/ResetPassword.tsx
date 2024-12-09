a2359b9595fc47622d90ccbab3f569eab44e3595
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-09 16:51:41 +0100

    feat: create ResetPassword route component

---
+++ src/routes/resetPassword/ResetPassword.tsx
@@ -0,0 +1,13 @@
+import './ResetPassword.scss';
+
+const ResetPassword = () => {
+  return (
+    <div className='resetPassword'>
+      <div className='resetPassword__container'>
+        <div className='resetPassword__wrapper'>ResetPassword</div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
