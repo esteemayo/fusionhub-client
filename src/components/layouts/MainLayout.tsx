d6c92bb3f97775dbf698a969ebc1ffd6b8c309f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 01:50:30 +0100

    feat: add ScrollToTop component

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,12 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
 
+import ScrollToTop from '../scrollToTop/ScrollToTop';
+
 const MainLayout = () => {
   return (
     <main>
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollToTop />
     </main>
   );
 };
