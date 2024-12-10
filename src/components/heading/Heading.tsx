b2daed5ae4767b9a4d6ff961f68406d85d88fe7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 17:52:26 +0100

    fix: modify

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,10 +1,18 @@
+import { useMemo } from 'react';
+
+import { HeadingProps } from '../../types';
+
 import './Heading.scss';
 
-const Heading = () => {
+const Heading = ({ title, subtitle, center }: HeadingProps) => {
+  const headingClasses = useMemo(() => {
+    return center ? 'heading center' : 'heading';
+  }, [center]);
+
   return (
-    <div className='heading'>
-      <h1 className='heading__main'></h1>
-      <h2 className='heading__sub'></h2>
+    <div className={headingClasses}>
+      <h1 className='heading__main'>{title}</h1>
+      <h2 className='heading__sub'>{subtitle}</h2>
     </div>
   );
 };
