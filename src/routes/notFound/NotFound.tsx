2abd47eaf705337f46e69960edd383665fc8d097
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 17:56:42 +0100

    feat: add markup

--- src/routes/notFound/NotFound.tsx
+++ src/routes/notFound/NotFound.tsx
@@ -1,10 +1,19 @@
+import Heading from '../../components/heading/Heading';
+
 import './NotFound.scss';
 
 const NotFound = () => {
   return (
     <div className='notFound'>
       <div className='notFound__container'>
-        <div className='notFound__wrapper'>NotFound</div>
+        <div className='notFound__wrapper'>
+          <img src='' alt='404' className='notFound__img' />
+          <Heading
+            title='Page not found'
+            subtitle='Please! Try again later'
+            center
+          />
+        </div>
       </div>
     </div>
   );
