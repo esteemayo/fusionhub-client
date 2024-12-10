43c6ae442024617d6892cbf9a78a215e5a19fa10
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 02:14:11 +0100

    fix: refactor

--- src/components/backToTop/BackToTop.tsx
+++ src/components/backToTop/BackToTop.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import './BackToTop.scss';
 
@@ -22,24 +22,26 @@ const BackToTop = () => {
     });
   };
 
+  const scrollClasses = useMemo(() => {
+    return isVisible ? 'scroll show' : 'scroll hide';
+  }, [isVisible]);
+
   return (
-    <div className='scroll'>
-      {isVisible && (
-        <button type='button' className='scroll__btn' onClick={handleScroll}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='currentColor'
-            className='size-6'
-          >
-            <path
-              fillRule='evenodd'
-              d='M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z'
-              clipRule='evenodd'
-            />
-          </svg>
-        </button>
-      )}
+    <div className={scrollClasses}>
+      <button type='button' className='scroll__btn' onClick={handleScroll}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          className='size-6'
+        >
+          <path
+            fillRule='evenodd'
+            d='M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </button>
     </div>
   );
 };
