feeaf8762f79c03ea714b148527d45448e0c1ae7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 02:08:37 +0100

    feat: add handleScroll method

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -1,22 +1,45 @@
+import { useState } from 'react';
+
 import './ScrollToTop.scss';
 
 const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  window.onscroll = () => {
+    setIsVisible(() => {
+      return window.scrollY > 300 ? true : false;
+    });
+
+    return window.onscroll === null;
+  };
+
+  const handleScroll = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className='scroll'>
-      <button type='button' className='scroll__btn'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          className='size-6'
-        >
-          <path
-            fillRule='evenodd'
-            d='M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z'
-            clipRule='evenodd'
-          />
-        </svg>
-      </button>
+      {isVisible && (
+        <button type='button' className='scroll__btn' onClick={handleScroll}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            className='size-6'
+          >
+            <path
+              fillRule='evenodd'
+              d='M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </button>
+      )}
     </div>
   );
 };
