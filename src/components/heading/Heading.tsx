91dea3a58480c9d956834eae4def1d40c5c1acd2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 17:45:18 +0100

    feat: create Heading component

---
+++ src/components/heading/Heading.tsx
@@ -0,0 +1,12 @@
+import './Heading.scss';
+
+const Heading = () => {
+  return (
+    <div className='heading'>
+      <h1 className='heading__main'></h1>
+      <h2 className='heading__sub'></h2>
+    </div>
+  );
+};
+
+export default Heading;
