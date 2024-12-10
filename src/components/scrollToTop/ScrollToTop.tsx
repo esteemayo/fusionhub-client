0581767274fc4659e1238cd76e555e0d97f95a32
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-10 01:49:18 +0100

    feat: add markup

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -1,7 +1,24 @@
 import './ScrollToTop.scss';
 
 const ScrollToTop = () => {
-  return <div className='scroll'>ScrollToTop</div>;
+  return (
+    <div className='scroll'>
+      <button type='button'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          className='size-6'
+        >
+          <path
+            fillRule='evenodd'
+            d='M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default ScrollToTop;
