267773c0fcef5eef98f83476940b7e65f644432c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-11 03:14:00 +0100

    feat: create a sidebar slice

---
+++ src/features/sidebar/sidebarSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isOpen: false,
+};
+
+const sidebarSlice = createSlice({
+  name: 'sidebar',
+  initialState,
+  reducers: {
+    onOpen: (state) => {
+      state.isOpen = true;
+    },
+    onClose: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { onClose, onOpen } = sidebarSlice.actions;
+
+export default sidebarSlice.reducer;
