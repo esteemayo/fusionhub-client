dd4d890bc9beeee4e21511ac16371ae2c50e5980
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 21:50:01 +0100

    feat: add markup

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
+
 import './UserMenu.scss';
 
 const UserMenu = () => {
   return (
     <aside className='userMenu'>
-      <div className='userMenu__container'>UserMenu</div>
+      <div className='userMenu__container'>
+        <ul className='userMenu__list'>
+          <li className='userMenu__list--item'>
+            <Link to='/account'>Account</Link>
+          </li>
+          <li className='userMenu__list--item'>
+            <span>John doe</span>
+          </li>
+          <li className='userMenu__list--item'>
+            <button type='button'>Logout</button>
+          </li>
+        </ul>
+      </div>
     </aside>
   );
 };
