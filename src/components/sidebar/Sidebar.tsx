539ebf874036c460e895822dcfdef4c2fa28e0e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 02:47:03 +0100

    feat: add markup

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 
 import { useAppSelector } from '../../hooks/hooks';
 
@@ -13,7 +14,29 @@ const Sidebar = () => {
 
   return (
     <aside className={sidebarClasses}>
-      <div className='sidebar__container'>Sidebar</div>
+      <div className='sidebar__container'>
+        <div className='sidebar__wrapper'>
+          <div className='sidebar__logo'>
+            <Link to='/'>Logo</Link>
+          </div>
+          <button className='sidebar__closeBtn'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='size-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M6 18 18 6M6 6l12 12'
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </aside>
   );
 };
