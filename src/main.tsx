327944d264de29b353bf247fc49af5e39a38e898
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 02:31:28 +0100

    feat: wrap the application with redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import { store } from './features/store.ts';
 
 import './global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
