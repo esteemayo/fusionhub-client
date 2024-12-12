7d3bd69682b57865c52c7e4656af0d3b08d80f69
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 21:53:41 +0100

    fix: modify

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -10,13 +10,13 @@ const UserMenu = () => {
           <li className='userMenu__list--item'>
             <Link to='/account'>Account</Link>
           </li>
-          <li className='userMenu__list--item'>
-            <span>John doe</span>
-          </li>
-          <li className='userMenu__list--item'>
-            <button type='button'>Logout</button>
-          </li>
         </ul>
+        <div className='userMenu__wrapper'>
+          <span className='username'>John doe</span>
+          <button type='button' className='logoutBtn'>
+            Logout
+          </button>
+        </div>
       </div>
     </aside>
   );
