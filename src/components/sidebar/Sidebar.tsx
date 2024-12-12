a2a5b9d8be22cac4d18e4711c3a76c636d35929c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 02:43:37 +0100

    feat: add sidebar classes dynamically

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,18 @@
+import { useMemo } from 'react';
+
+import { useAppSelector } from '../../hooks/hooks';
+
 import './Sidebar.scss';
 
 const Sidebar = () => {
+  const { isOpen } = useAppSelector((state) => ({ ...state.sidebar }));
+
+  const sidebarClasses = useMemo(() => {
+    return isOpen ? 'sidebar show' : 'sidebar hide';
+  }, [isOpen]);
+
   return (
-    <aside className='sidebar'>
+    <aside className={sidebarClasses}>
       <div className='sidebar__container'>Sidebar</div>
     </aside>
   );
