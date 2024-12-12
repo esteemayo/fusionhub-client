577c0c1bf0b7d117de4c5ba71758f6d65e9a33cf
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-12 21:33:46 +0100

    feat: add UserMenu component

---
+++ src/components/userMenu/UserMenu.tsx
@@ -0,0 +1,11 @@
+import './UserMenu.scss';
+
+const UserMenu = () => {
+  return (
+    <aside className='userMenu'>
+      <div className='userMenu__container'>UserMenu</div>
+    </aside>
+  );
+};
+
+export default UserMenu;
