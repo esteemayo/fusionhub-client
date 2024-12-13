c224b9a45af065769d5b08b0539ce9d8272c118b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-13 18:54:52 +0100

    feat: create menuItems navigation links

---
+++ src/data/index.ts
@@ -0,0 +1,22 @@
+export const menuItems = [
+  {
+    id: 1,
+    url: '/',
+    label: 'Home',
+  },
+  {
+    id: 2,
+    url: '/posts',
+    label: 'Posts',
+  },
+  {
+    id: 3,
+    url: '/about',
+    label: 'About',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    label: 'Contact',
+  },
+];
