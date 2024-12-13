7770663c5a790fc8cbb1c29f0f40a175887753c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-13 19:03:55 +0100

    fix: refactor

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { onClose } from '../../features/sidebar/sidebarSlice';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 
+import { menuItems } from '../../data';
+
 import './Sidebar.scss';
 
 const Sidebar = () => {
@@ -48,12 +50,14 @@ const Sidebar = () => {
         </div>
         <div className='sidebar__box'>
           <ul className='sidebar__menu'>
-            <li className='sidebar__menu--item'>
-              <Link to='/'>Home</Link>
-            </li>
-            <li className='sidebar__menu--item'>
-              <Link to='/posts'>Posts</Link>
-            </li>
+            {menuItems.map((menu) => {
+              const { id, url, label } = menu;
+              return (
+                <li key={id} className='sidebar__menu--item'>
+                  <Link to={url}>{label}</Link>
+                </li>
+              );
+            })}
           </ul>
           <div className='sidebar__accountWrap'>
             {user && (
