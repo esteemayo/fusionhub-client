a3e9ba6ca5dbd2d25ef9e7d4663a7d78be318b9d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 00:45:04 +0100

    feat: change component' name

---
+++ src/routes/postDetails/PostDetails.tsx
@@ -0,0 +1,11 @@
+import './PostDetails.scss';
+
+const PostDetails = () => {
+  return (
+    <div className='postDetails'>
+      <div className='postDetails__container'>PostDetails</div>
+    </div>
+  );
+};
+
+export default PostDetails;
