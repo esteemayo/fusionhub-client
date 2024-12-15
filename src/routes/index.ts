2b2ac0d89bab6def906a47ec1b9b5430b74e51ed
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 00:46:30 +0100

    fix: modify

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,11 @@ import NotFound from './notFound/NotFound';
 import Home from './home/Home';
 import Register from './register/Register';
 import Login from './login/Login';
-import ResetPassword from './resetPassword/ResetPassword';
-import Posts from './posts/Posts';
-import ForgotPassword from './forgotPassword/ForgotPassword';
-import Account from './account/Account';
 import PostDetails from './postDetails/PostDetails';
+import Posts from './posts/Posts';
+import ResetPassword from './resetPassword/ResetPassword';
+import Account from './account/Account';
+import ForgotPassword from './forgotPassword/ForgotPassword';
 
 export {
   Account,
