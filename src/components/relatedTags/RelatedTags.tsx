fe6647b0480d622620dc438a09ce432ab1031f94
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 14:42:02 +0100

    feat: create RelatedTags component

---
+++ src/components/relatedTags/RelatedTags.tsx
@@ -0,0 +1,16 @@
+import './RelatedTags.scss';
+
+const RelatedTags = () => {
+  return (
+    <section className='relatedTags'>
+      <span className='relatedTags__item'>Travel</span>
+      <span className='relatedTags__item'>Lifestyle</span>
+      <span className='relatedTags__item'>Photo</span>
+      <span className='relatedTags__item'>Adventures</span>
+      <span className='relatedTags__item'>Musician</span>
+      <span className='relatedTags__item'>Food</span>
+    </section>
+  );
+};
+
+export default RelatedTags;
