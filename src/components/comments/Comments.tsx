00dc5cccbf8cbfcb21ce81c340f5ec128f641c74
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 15:06:18 +0100

    feat: create Comments component

---
+++ src/components/comments/Comments.tsx
@@ -0,0 +1,11 @@
+import './Comments.scss';
+
+const Comments = () => {
+  return (
+    <section className='comments'>
+      <div className='comments__container'>Comments</div>
+    </section>
+  );
+};
+
+export default Comments;
