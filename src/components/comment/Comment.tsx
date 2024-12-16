d4527684335d0a2d3a7755779b525028475fde70
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 15:33:07 +0100

    feat: create Comment component

---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,12 @@
+import './Comment.scss';
+
+const Comment = () => {
+  return (
+    <div className='comment'>
+      <h3 className='comment__heading'>Comment</h3>
+      <div className='comment__container'>Comment card</div>
+    </div>
+  );
+};
+
+export default Comment;
--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -5,7 +5,6 @@ import './Comments.scss';
 const Comments = () => {
   return (
     <section className='comments'>
-      <h3 className='comments__heading'>Comments</h3>
       <div className='comments__container'>
         <CommentForm />
       </div>
