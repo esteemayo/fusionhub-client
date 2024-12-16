2f34869dcca36e06918b55e03ba927fdf80b06e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 01:38:49 +0100

    feat: add styling

--- src/routes/postDetails/PostDetails.tsx
+++ src/routes/postDetails/PostDetails.tsx
@@ -6,7 +6,10 @@ const PostDetails = () => {
   return (
     <div className='postDetails'>
       <Hero />
-      <div className='postDetails__container'>PostDetails</div>
+      <div className='postDetails__container'>
+        <div className='postDetails__container--left'>PostDetails Left</div>
+        <div className='postDetails__container--right'>PostDetails Right</div>
+      </div>
     </div>
   );
 };
