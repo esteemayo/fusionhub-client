6db2680715df6e4353b690a87a5224590fc7137e
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 16:21:17 +0100

    feat: add images

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -3,8 +3,39 @@ import './Comment.scss';
 const Comment = () => {
   return (
     <div className='comment'>
-      <h3 className='comment__heading'>Comment</h3>
-      <div className='comment__container'>Comment card</div>
+      <h4 className='comment__heading'>Comment</h4>
+      <div className='comment__card'>
+        <div className='comment__user'>
+          <img
+            src='/user.jpeg'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+        </div>
+        <div className='comment__details'>
+          <div className='comment__dateBox'>
+            <time
+              dateTime={new Date().toDateString()}
+              className='comment__dateBox--time'
+            >
+              {new Date().toDateString()}
+            </time>
+            <button type='button' className='comment__dateBox--reply'>
+              logo
+              <span>Reply</span>
+            </button>
+          </div>
+          <h5 className='comment__details--username'>Rosalina pong</h5>
+          <p className='comment__details--desc'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            minus soluta architecto aspernatur repudiandae provident odio
+            voluptatibus quia! Beatae aspernatur alias repellat dicta, eum
+            voluptas commodi voluptate reiciendis reprehenderit laboriosam.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
