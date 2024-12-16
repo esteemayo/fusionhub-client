13cae7c64c8b8d1b5a4e3f75b7e8536a5a579a74
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 09:30:51 +0100

    feat: add styling

--- src/components/postDetail/PostDetail.tsx
+++ src/components/postDetail/PostDetail.tsx
@@ -38,7 +38,7 @@ const PostDetail = () => {
                   d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
                 />
               </svg>
-              <span>19 hours ago</span>
+              <time dateTime={new Date().toDateString()}>19 hours ago</time>
             </div>
           </div>
           <div className='postDetail__actions'>
