5060edd98a62fd80b51fd730ccb88977ea82d61b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 01:45:34 +0100

    feat: add markup

--- src/components/postDetail/PostDetail.tsx
+++ src/components/postDetail/PostDetail.tsx
@@ -1,7 +1,22 @@
 import './PostDetail.scss';
 
 const PostDetail = () => {
-  return <section className='postDetail'>PostDetail</section>;
+  return (
+    <section className='postDetail'>
+      <div className='postDetail__container'>
+        <div className='postDetail__user'>
+          <div className='postDetail__username'>
+            icon
+            <span>John doe</span>
+          </div>
+          <div className='postDetail__date'>
+            icon
+            <span>19 hours ago</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PostDetail;
