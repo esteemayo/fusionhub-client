e62552a5941a2294c467e3a59f58a8ca1494d205
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-16 01:09:40 +0100

    feat: add markup

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,16 @@
 import './Hero.scss';
 
 const Hero = () => {
-  return <header className='hero'>Hero</header>;
+  return (
+    <header className='hero'>
+      <div className='hero__breadCrumbs'>
+        <span>Home</span>
+        {'|'}
+        <span>Post details</span>
+      </div>
+      <h1 className='hero__heading'>Go boating and experience</h1>
+    </header>
+  );
 };
 
 export default Hero;
