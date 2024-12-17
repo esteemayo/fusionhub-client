320786f5c9c4dba9df46dbc910f8d3a64596f3c6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-17 18:05:43 +0100

    feat: add markup

--- src/components/relatedPost/RelatedPost.tsx
+++ src/components/relatedPost/RelatedPost.tsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom';
+
 import './RelatedPost.scss';
 
 const RelatedPost = () => {
   return (
     <article className='relatedPost'>
-      <div className='relatedPost__container'>RelatedPost</div>
+      <div className='relatedPost__container'>
+        <img
+          src='/post-1.jpg'
+          width={250}
+          height={135}
+          alt='image'
+          className='relatedPost__container--img'
+        />
+        <div className='relatedpost__container--footer'>
+          <Link to='#'>
+            What chelsea fans absolutely loved about Pedro Neto's performance
+          </Link>
+          <span>The chelsea chronicle</span>
+          <time dateTime=''>43 mins ago</time>
+        </div>
+      </div>
     </article>
   );
 };
