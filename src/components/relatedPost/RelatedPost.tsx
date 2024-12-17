e2c65964a3c3200507535c48753d64137a564435
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-17 17:37:07 +0100

    feat: create RelatedPost component

---
+++ src/components/relatedPost/RelatedPost.tsx
@@ -0,0 +1,11 @@
+import './RelatedPost.scss';
+
+const RelatedPost = () => {
+  return (
+    <article className='relatedPost'>
+      <div className='relatedPost__container'>RelatedPost</div>
+    </article>
+  );
+};
+
+export default RelatedPost;
