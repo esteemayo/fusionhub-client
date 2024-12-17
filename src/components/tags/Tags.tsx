dd04cb20721954e15815351355a27adbd31e1eb7
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-17 16:38:45 +0100

    feat: create Tags component

---
+++ src/components/tags/Tags.tsx
@@ -0,0 +1,27 @@
+import './Tags.scss';
+
+const Tags = () => {
+  return (
+    <aside className='tags'>
+      <div className='tags__container'>
+        <h2 className='tags__container--heading'>Tags</h2>
+        <div className='tags__wrapper'>
+          <span className='tags__wrapper--item'>Travel</span>
+          <span className='tags__wrapper--item'>Lifestyle</span>
+          <span className='tags__wrapper--item'>Photo</span>
+          <span className='tags__wrapper--item'>Adventures</span>
+          <span className='tags__wrapper--item'>Musician</span>
+          <span className='tags__wrapper--item'>Food</span>
+          <span className='tags__wrapper--item'>Travel</span>
+          <span className='tags__wrapper--item'>Lifestyle</span>
+          <span className='tags__wrapper--item'>Photo</span>
+          <span className='tags__wrapper--item'>Adventures</span>
+          <span className='tags__wrapper--item'>Musician</span>
+          <span className='tags__wrapper--item'>Food</span>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Tags;
