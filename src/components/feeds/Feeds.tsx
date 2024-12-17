a39d8e3e030cb72ebb378749adc59e914358626f
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-17 16:11:33 +0100

    fix: refactor

--- src/components/feeds/Feeds.tsx
+++ src/components/feeds/Feeds.tsx
@@ -1,4 +1,7 @@
+import { format } from 'timeago.js';
 import { Link } from 'react-router-dom';
+
+import { feedItems } from '../../data';
 
 import './Feeds.scss';
 
@@ -7,179 +10,48 @@ const Feeds = () => {
     <aside className='feeds'>
       <div className='feeds__container'>
         <h2 className='feeds__container-heading'>Feeds</h2>
-        <article className='feeds__card'>
-          <div className='feeds__card-wrapper'>
-            <img
-              src='/post-1.jpg'
-              alt='image'
-              width={80}
-              height={80}
-              className='feeds__card-wrapper--img'
-            />
-          </div>
-          <div className='feeds__card-box'>
-            <h3 className='feeds__card-box--title'>
-              <Link to='/posts/test'>Alonso Kelina Falao Asiano Pero</Link>
-            </h3>
-            <time dateTime='' className='feeds__card-box--date'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-                />
-              </svg>
-              <span>6 hours ago</span>
-            </time>
-          </div>
-        </article>
-        <article className='feeds__card'>
-          <div className='feeds__card-wrapper'>
-            <img
-              src='/post-2.jpeg'
-              alt='image'
-              width={80}
-              height={80}
-              className='feeds__card-wrapper--img'
-            />
-          </div>
-          <div className='feeds__card-box'>
-            <h3 className='feeds__card-box--title'>
-              <Link to='/posts/test'>
-                It is a long established fact that a reader
-              </Link>
-            </h3>
-            <time dateTime='' className='feeds__card-box--date'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-                />
-              </svg>
-              <span>6 hours ago</span>
-            </time>
-          </div>
-        </article>
-        <article className='feeds__card'>
-          <div className='feeds__card-wrapper'>
-            <img
-              src='/post-3.jpeg'
-              alt='image'
-              width={80}
-              height={80}
-              className='feeds__card-wrapper--img'
-            />
-          </div>
-          <div className='feeds__card-box'>
-            <h3 className='feeds__card-box--title'>
-              <Link to='/posts/test'>
-                Many desktop publish packages and web
-              </Link>
-            </h3>
-            <time dateTime='' className='feeds__card-box--date'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-                />
-              </svg>
-              <span>6 hours ago</span>
-            </time>
-          </div>
-        </article>
-        <article className='feeds__card'>
-          <div className='feeds__card-wrapper'>
-            <img
-              src='/post-4.webp'
-              alt='image'
-              width={80}
-              height={80}
-              className='feeds__card-wrapper--img'
-            />
-          </div>
-          <div className='feeds__card-box'>
-            <h3 className='feeds__card-box--title'>
-              <Link to='/posts/test'>
-                Various versions have evolved over the years
-              </Link>
-            </h3>
-            <time dateTime='' className='feeds__card-box--date'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-                />
-              </svg>
-              <span>6 hours ago</span>
-            </time>
-          </div>
-        </article>
-        <article className='feeds__card'>
-          <div className='feeds__card-wrapper'>
-            <img
-              src='/post-5.jpeg'
-              alt='image'
-              width={80}
-              height={80}
-              className='feeds__card-wrapper--img'
-            />
-          </div>
-          <div className='feeds__card-box'>
-            <h3 className='feeds__card-box--title'>
-              <Link to='/posts/test'>
-                Photo booth anim 8-bit PBSC 3 wolf moon.
-              </Link>
-            </h3>
-            <time dateTime='' className='feeds__card-box--date'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-                />
-              </svg>
-              <span>6 hours ago</span>
-            </time>
-          </div>
-        </article>
+        {feedItems.map((feed) => {
+          return (
+            <article key={feed.id} className='feeds__card'>
+              <div className='feeds__card-wrapper'>
+                {feed.img && (
+                  <img
+                    src={feed.img}
+                    alt='image'
+                    width={80}
+                    height={80}
+                    className='feeds__card-wrapper--img'
+                  />
+                )}
+              </div>
+              <div className='feeds__card-box'>
+                <h3 className='feeds__card-box--title'>
+                  <Link to='/posts/test'>{feed.title}</Link>
+                </h3>
+                <time
+                  dateTime={feed.createdAt}
+                  className='feeds__card-box--date'
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+                    />
+                  </svg>
+                  <span>{format(feed.createdAt)}</span>
+                </time>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </aside>
   );
