0ac91bd660c463f3a6a3e864e5e23f94df32ae78
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-17 17:01:57 +0100

    fix: refactor

--- src/components/relatedTags/RelatedTags.tsx
+++ src/components/relatedTags/RelatedTags.tsx
@@ -1,3 +1,5 @@
+import { relatedTags } from '../../data';
+
 import './RelatedTags.scss';
 
 const RelatedTags = () => {
@@ -5,12 +7,14 @@ const RelatedTags = () => {
     <section className='relatedTags'>
       <h3 className='relatedTags__heading'>Related tags</h3>
       <div className='relatedTags__container'>
-        <span className='relatedTags__container--item'>Travel</span>
-        <span className='relatedTags__container--item'>Lifestyle</span>
-        <span className='relatedTags__container--item'>Photo</span>
-        <span className='relatedTags__container--item'>Adventures</span>
-        <span className='relatedTags__container--item'>Musician</span>
-        <span className='relatedTags__container--item'>Food</span>
+        {relatedTags.map((tag) => {
+          const { id, label } = tag;
+          return (
+            <span key={id} className='relatedTags__container--item'>
+              {label}
+            </span>
+          );
+        })}
       </div>
     </section>
   );
