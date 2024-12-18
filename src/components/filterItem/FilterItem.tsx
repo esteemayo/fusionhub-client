6cf20274674cb7caf1454811ecca415b2ae26db3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 19:19:56 +0100

    feat: add styling

--- src/components/filterItem/FilterItem.tsx
+++ src/components/filterItem/FilterItem.tsx
@@ -5,40 +5,52 @@ const FilterItem = () => {
     <div className='filterItem'>
       <div className='filterItem__container'>
         <div className='filterItem__group'>
-          <label className='filterItem__group--label'>Newest</label>
           <input
             type='radio'
-            name='name'
+            name='sort'
+            id='newest'
             value='newest'
             className='filterItem__group--control'
           />
+          <label htmlFor='newest' className='filterItem__group--label'>
+            Newest
+          </label>
         </div>
         <div className='filterItem__group'>
-          <label className='filterItem__group--label'>Most popular</label>
           <input
             type='radio'
-            name='name'
+            name='sort'
+            id='popular'
             value='popular'
             className='filterItem__group--control'
           />
+          <label htmlFor='popular' className='filterItem__group--label'>
+            Most popular
+          </label>
         </div>
         <div className='filterItem__group'>
-          <label className='filterItem__group--label'>Trending</label>
           <input
             type='radio'
-            name='name'
+            name='sort'
+            id='trending'
             value='trending'
             className='filterItem__group--control'
           />
+          <label htmlFor='trending' className='filterItem__group--label'>
+            Trending
+          </label>
         </div>
         <div className='filterItem__group'>
-          <label className='filterItem__group--label'>Oldest</label>
           <input
             type='radio'
-            name='name'
+            name='sort'
+            id='oldest'
             value='oldest'
             className='filterItem__group--control'
           />
+          <label htmlFor='oldest' className='filterItem__group--label'>
+            Oldest
+          </label>
         </div>
       </div>
     </div>
