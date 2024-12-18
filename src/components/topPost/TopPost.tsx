d90f59991ea812bc467199bd49b18e19f22a04df
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 17:41:56 +0100

    feat: add styling

--- src/components/topPost/TopPost.tsx
+++ src/components/topPost/TopPost.tsx
@@ -7,7 +7,7 @@ const TopPost = () => {
         <span className='topPost__container--number'>1</span>
         <div className='topPost__wrapper'>
           <span className='topPost__wrapper--title'>
-            Omnis velit exercitationem soluta nam nemo error facere fugit minus!
+            Omnis velit exercitationem soluta nam nemo!
           </span>
           <div className='topPost__wrapper--category'>
             <span>Food, Travel</span>
