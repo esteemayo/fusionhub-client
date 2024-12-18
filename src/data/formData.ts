f2b7e6ad154018569281d3faa29eff08f6ccbb15
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 19:43:08 +0100

    feat: add filters data object array

---
+++ src/data/formData.ts
@@ -0,0 +1,26 @@
+export const filters = [
+  {
+    id: 'newest',
+    name: 'sort',
+    value: 'newest',
+    label: 'Newest',
+  },
+  {
+    id: 'popular',
+    name: 'sort',
+    value: 'popular',
+    label: 'Most popular',
+  },
+  {
+    id: 'trending',
+    name: 'sort',
+    value: 'trending',
+    label: 'Trending',
+  },
+  {
+    id: 'oldest',
+    name: 'sort',
+    value: 'oldest',
+    label: 'Oldest',
+  },
+];
