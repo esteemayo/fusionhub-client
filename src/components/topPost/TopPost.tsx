c0fdd03ac1fb85391b422eb738c99a17a2e9f0b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 17:51:46 +0100

    feat: add styling

--- src/components/topPost/TopPost.tsx
+++ src/components/topPost/TopPost.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './TopPost.scss';
 
 const TopPost = () => {
@@ -7,7 +9,9 @@ const TopPost = () => {
         <span className='topPost__container--number'>1</span>
         <div className='topPost__wrapper'>
           <span className='topPost__wrapper--title'>
-            Omnis velit exercitationem soluta nam nemo!
+            <Link to={`/posts/slug`}>
+              Omnis velit exercitationem soluta nam nemo!
+            </Link>
           </span>
           <div className='topPost__wrapper--category'>
             <span>Food, Travel</span>
