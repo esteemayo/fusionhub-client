13943f551020d5f6e1b01ed327060643921c7027
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 17:06:21 +0100

    feat: add TopPosts component

---
+++ src/components/topPosts/TopPosts.tsx
@@ -0,0 +1,13 @@
+import './TopPosts.scss';
+
+const TopPosts = () => {
+  return (
+    <aside className='topPosts'>
+      <div className='topPosts__container'>
+        <h2 className='topPosts__container--heading'>Top posts</h2>
+      </div>
+    </aside>
+  );
+};
+
+export default TopPosts;
