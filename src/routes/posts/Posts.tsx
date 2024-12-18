4d22f40378781519478ade32059a7ef2c30a2dc6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 16:35:58 +0100

    feat: add markup

--- src/routes/posts/Posts.tsx
+++ src/routes/posts/Posts.tsx
@@ -3,7 +3,10 @@ import './Posts.scss';
 const Posts = () => {
   return (
     <div className='posts'>
-      <div className='posts__container'>Posts</div>
+      <div className='posts__container'>
+        <div className='posts__container--left'>Posts Left</div>
+        <div className='posts__container--right'>Posts Right</div>
+      </div>
     </div>
   );
 };
