61679993228eca7a66d170551cee050a7387b736
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-18 18:55:32 +0100

    feat: create Filter component

---
+++ src/components/filter/Filter.tsx
@@ -0,0 +1,13 @@
+import './Filter.scss';
+
+const Filter = () => {
+  return (
+    <aside className='filter'>
+      <div className='filter__container'>
+        <h2 className='filter__container--heading'>Filter</h2>
+      </div>
+    </aside>
+  );
+};
+
+export default Filter;
