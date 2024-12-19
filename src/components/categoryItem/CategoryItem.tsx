847359f59374c72132e02475f04d01c4983f0ea6
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 14:09:32 +0100

    fix: modify

--- src/components/categoryItem/CategoryItem.tsx
+++ src/components/categoryItem/CategoryItem.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 
 import { CategoryItemProps } from '../../types';
 
@@ -16,7 +17,9 @@ const CategoryItem = ({ name, total }: CategoryItemProps) => {
 
   return (
     <article className='categoryItem'>
-      <span className='categoryItem__label'>{name}</span>
+      <span className='categoryItem__label'>
+        <Link to={`/posts?category=${name}`}>{name}</Link>
+      </span>
       <span className='categoryItem__total'>({formattedTotal})</span>
     </article>
   );
