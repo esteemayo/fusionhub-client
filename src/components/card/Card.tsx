d2f79da12c41e09b6dfe34a707db4b5d928ee4c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 14:55:23 +0100

    feat: add markup

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './Card.scss';
 
 const Card = () => {
@@ -10,6 +12,24 @@ const Card = () => {
         height={300}
         className='card__img'
       />
+      <div className='card__footer'>
+        <div className='card__footer--container'>
+          <h3 className='card__footer--container-title'>
+            <Link to={`/posts/slug`}>
+              It is a long established fact that a reader
+            </Link>
+          </h3>
+          <p className='card__footer--container-desc'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit...
+          </p>
+        </div>
+        <div className='card__footer--wrapper'>
+          <span className='card__footer--wrapper-category'>Adventures</span>
+          <time dateTime='' className='card__footer--wrapper-time'>
+            2 days ago
+          </time>
+        </div>
+      </div>
     </article>
   );
 };
