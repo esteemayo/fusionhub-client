f2914a26c9af32a18b98be8cc4ac9ca438354e7d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 15:16:30 +0100

    feat: add styling

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -16,7 +16,7 @@ const Card = () => {
         <div className='card__footer--container'>
           <h3 className='card__footer--container-title'>
             <Link to={`/posts/slug`}>
-              It is a long established fact that a reader
+              It is a long established fact that a reader and
             </Link>
           </h3>
           <p className='card__footer--container-desc'>
