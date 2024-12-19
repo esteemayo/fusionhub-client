a0900179a628b1ec05b15d9d79fcf49fec7e8f06
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 14:07:12 +0100

    feat:format number

--- src/components/categoryItem/CategoryItem.tsx
+++ src/components/categoryItem/CategoryItem.tsx
@@ -1,12 +1,23 @@
+import { useMemo } from 'react';
+
 import { CategoryItemProps } from '../../types';
 
 import './CategoryItem.scss';
 
 const CategoryItem = ({ name, total }: CategoryItemProps) => {
+  const formattedTotal = useMemo(() => {
+    if (total < 10) {
+      const formattedNumber = total.toString().padStart(2, '0');
+      return formattedNumber;
+    }
+
+    return total;
+  }, [total]);
+
   return (
     <article className='categoryItem'>
       <span className='categoryItem__label'>{name}</span>
-      <span className='categoryItem__total'>({total})</span>
+      <span className='categoryItem__total'>({formattedTotal})</span>
     </article>
   );
 };
