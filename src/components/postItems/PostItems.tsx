d7c9521034edb0177fdfdfabe2178bf922f61e9d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 14:36:52 +0100

    feat: create PostItems component

---
+++ src/components/postItems/PostItems.tsx
@@ -0,0 +1,11 @@
+import './PostItems.scss';
+
+const PostItems = () => {
+  return (
+    <section className='postItems'>
+      <div className='postItems__container'>PostItems</div>
+    </section>
+  );
+};
+
+export default PostItems;
