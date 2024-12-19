4e2dacd4e8ff3799967a74437b26f7f5556d67b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 13:31:20 +0100

    fix: modify

--- src/components/filterItem/FilterItem.tsx
+++ src/components/filterItem/FilterItem.tsx
@@ -16,42 +16,6 @@ const FilterItem = () => {
             Newest
           </label>
         </div>
-        <div className='filterItem__group'>
-          <input
-            type='radio'
-            name='sort'
-            id='popular'
-            value='popular'
-            className='filterItem__group--control'
-          />
-          <label htmlFor='popular' className='filterItem__group--label'>
-            Most popular
-          </label>
-        </div>
-        <div className='filterItem__group'>
-          <input
-            type='radio'
-            name='sort'
-            id='trending'
-            value='trending'
-            className='filterItem__group--control'
-          />
-          <label htmlFor='trending' className='filterItem__group--label'>
-            Trending
-          </label>
-        </div>
-        <div className='filterItem__group'>
-          <input
-            type='radio'
-            name='sort'
-            id='oldest'
-            value='oldest'
-            className='filterItem__group--control'
-          />
-          <label htmlFor='oldest' className='filterItem__group--label'>
-            Oldest
-          </label>
-        </div>
       </div>
     </div>
   );
