1a9530fad0f2b94eac121208e9fd92182d50939a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-19 13:35:36 +0100

    fix: refactor

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,5 +1,7 @@
 import FilterItem from '../filterItem/FilterItem';
 
+import { filters } from '../../data/formData';
+
 import './Filter.scss';
 
 const Filter = () => {
@@ -7,7 +9,9 @@ const Filter = () => {
     <aside className='filter'>
       <div className='filter__container'>
         <h2 className='filter__container--heading'>Filter</h2>
-        <FilterItem />
+        {filters.map((filter) => {
+          return <FilterItem key={filter.id} {...filter} />;
+        })}
       </div>
     </aside>
   );
