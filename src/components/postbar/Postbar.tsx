ac973c94aec3297fae7a4059bfebf63c67c03196
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-20 14:14:40 +0100

    feat: add styling

--- src/components/postbar/Postbar.tsx
+++ src/components/postbar/Postbar.tsx
@@ -6,15 +6,17 @@ const Postbar = () => {
   return (
     <div className='postbar'>
       <div className='postbar__container'>
-        <h1 className='postbar__container--heading'>Posts</h1>
-        <div className='postbar__container--breadcrumbs'>
-          <ul className='postbar__lists'>
-            <li className='postbar__lists--item'>
-              <Link to='/' className='postbar__lists--item-link'>
-                Home
-              </Link>
-            </li>
-          </ul>
+        <div className='postbar__wrapper'>
+          <h1 className='postbar__wrapper--heading'>Posts</h1>
+          <div className='postbar__wrapper--breadcrumbs'>
+            <ul className='postbar__lists'>
+              <li className='postbar__lists--item'>
+                <Link to='/' className='postbar__lists--item-link'>
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
