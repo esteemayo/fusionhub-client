554e4c1e005f053669ad334e30cce99effa06c64
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-24 21:21:43 +0100

    feat: add classes

--- src/components/postClient/PostClient.tsx
+++ src/components/postClient/PostClient.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import TopPosts from '../topPosts/TopPosts';
 import Filter from '../filter/Filter';
 import Categories from '../categories/Categories';
@@ -5,9 +7,13 @@ import Search from '../search/Search';
 
 import './PostClient.scss';
 
-const PostClient = () => {
+const PostClient = ({ isOpen }: { isOpen: boolean }) => {
+  const postClientClasses = useMemo(() => {
+    return isOpen ? 'postClient show' : 'postClient hide';
+  }, [isOpen]);
+
   return (
-    <div className='postClient'>
+    <div className={postClientClasses}>
       <Search />
       <Filter />
       <Categories />
