4367447ab808669ed277054c06b947190be9fbe3
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-12-27 19:49:01 +0100

    fix: modify

--- src/components/feeds/Feeds.tsx
+++ src/components/feeds/Feeds.tsx
@@ -5,14 +5,14 @@ import './Feeds.scss';
 
 const Feeds = () => {
   return (
-    <aside className='feeds'>
+    <section className='feeds'>
       <div className='feeds__container'>
         <h2 className='feeds__container-heading'>Feeds</h2>
         {feedItems.map((feed) => {
           return <Feed key={feed.id} {...feed} />;
         })}
       </div>
-    </aside>
+    </section>
   );
 };
 
