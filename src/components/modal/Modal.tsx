29bf604ff70a4d6ed98d15ac862aa3be724bda45
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-01-05 17:02:26 +0100

    feat: add close button to Modal component and enhance styling for better usability

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -16,7 +16,26 @@ const Modal = () => {
               <button type='button' className='modal__buttons--btn'>
                 action
               </button>
+              Modal footer
             </div>
+          </div>
+          <div className='modal__close'>
+            <button type='button' className='modal__close--btn'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M6 18 18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
