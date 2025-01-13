18874025794d0b1bd6e0a5b5ad85f14b7ead8ba5
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-01-13 14:29:05 +0100

    feat: enhance ProfileSettings component; add header section, user details display, and improve layout and styling

--- src/routes/profileSettings/ProfileSettings.tsx
+++ src/routes/profileSettings/ProfileSettings.tsx
@@ -21,38 +21,61 @@ const ProfileSettings = () => {
   return (
     <div className='profileSettings'>
       <div className='profileSettings__container'>
+        <div className='profileSettings__header'>
+          <h1 className='profileSettings__header--heading'>My profile</h1>
+          <span className='profileSettings__header--text'>
+            Update your account information
+          </span>
+        </div>
         <div className='profileSettings__wrapper'>
           <div className='profileSettings__box'>
-            <img
-              src='/user-1.jpeg'
-              width={120}
-              height={120}
-              alt='avatar'
-              className='profileSettings__box--avatar'
-            />
-            <div className='profileSettings__box--icon'>
-              <label htmlFor='file'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='size-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z'
+            <div className='profileSettings__user'>
+              <div className='profileSettings__image'>
+                <img
+                  src='/user-1.jpeg'
+                  width={120}
+                  height={120}
+                  alt='avatar'
+                  className='profileSettings__image--avatar'
+                />
+                <div className='profileSettings__box--icon'>
+                  <label htmlFor='file'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth={1.5}
+                      stroke='currentColor'
+                      className='size-6'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z'
+                      />
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z'
+                      />
+                    </svg>
+                  </label>
+                  <input
+                    id='file'
+                    type='file'
+                    accept='image/*'
+                    ref={inputRef}
                   />
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z'
-                  />
-                </svg>
-              </label>
-              <input id='file' type='file' accept='image/*' ref={inputRef} />
+                </div>
+              </div>
+              <div className='profileSettings__details'>
+                <span className='profileSettings__details--name'>
+                  Elise beverley
+                </span>
+                <span className='profileSettings__details--bio'>
+                  I'm a Senior Frontend Developer based in U.S
+                </span>
+              </div>
             </div>
           </div>
           <div className='profileSettings__buttons'>
@@ -70,6 +93,14 @@ const ProfileSettings = () => {
         </div>
         <hr />
         <div className='profileSettings__data'>
+          <div className='profileSettings__info'>
+            <h2 className='profileSettings__info--heading'>
+              Personal information
+            </h2>
+            <span className='profileSettings__info--text'>
+              Update your personal information
+            </span>
+          </div>
           <form onSubmit={handleSubmit} className='profileSettings__form'>
             <div className='profileSettings__form--data'>
               <Input name='name' label='Name' placeholder='Name' />
