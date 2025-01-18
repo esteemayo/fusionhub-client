b52a097ee49e972262446c983128b2ffc78694e5
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-01-18 14:51:01 +0100

    feat: add AboutProfile component; implement styles and structure for user about section

---
+++ src/components/aboutProfile/AboutProfile.tsx
@@ -0,0 +1,26 @@
+import './AboutProfile.scss';
+
+const AboutProfile = () => {
+  return (
+    <div className='aboutProfile'>
+      <div className='aboutProfile__container'>
+        <h4 className='aboutProfile__container--heading'>About me</h4>
+        <p className='aboutProfile__container--text'>
+          I'm a Full-stack Developer based in United State of America. I
+          specialize in Frontend Developement, UX/UI design, brand strategy, and
+          Webflow development. I'm always striving to grow and learn something
+          new and i don't take myself too seriously.
+        </p>
+        <p className='aboutProfile__container--text'>
+          I'm passionate about helping startups grow, improve their customer
+          experience, and to raise venture capital through good design.
+        </p>
+        <button type='button' className='aboutProfile__container--btn'>
+          Read more
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AboutProfile;
--- src/routes/profile/Profile.tsx
+++ src/routes/profile/Profile.tsx
@@ -1,6 +1,7 @@
 import AccountHeading from '../../components/accountHeading/AccountHeading';
 import Banner from '../../components/banner/Banner';
 import ProfileDetails from '../../components/profileDetails/ProfileDetails';
+import AboutProfile from '../../components/aboutProfile/AboutProfile';
 
 import './Profile.scss';
 
@@ -17,22 +18,7 @@ const UserProfile = () => {
       <div className='profile__wrapper'>
         <Banner />
         <ProfileDetails />
-        <div className='profile__about'>
-          <h4 className='profile__about--heading'>About me</h4>
-          <p className='profile__about--text'>
-            I'm a Full-stack Developer based in United State of America. I
-            specialize in Frontend Developement, UX/UI design, brand strategy,
-            and Webflow development. I'm always striving to grow and learn
-            something new and i don't take myself too seriously.
-          </p>
-          <p className='profile__about--text'>
-            I'm passionate about helping startups grow, improve their customer
-            experience, and to raise venture capital through good design.
-          </p>
-          <button type='button' className='profile__about--btn'>
-            Read more
-          </button>
-        </div>
+        <AboutProfile />
       </div>
     </div>
   );
