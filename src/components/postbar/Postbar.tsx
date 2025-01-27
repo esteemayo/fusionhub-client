61cd3d43bf9eae042653ec995b4ed7e94323ebb2
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-01-27 02:09:03 +0100

    feat: add primary blue background color to various components for improved styling

--- src/components/postbar/Postbar.tsx
+++ src/components/postbar/Postbar.tsx
@@ -1,8 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { onOpen } from '../../features/postsMenu/postsMenuSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 
 import './Postbar.scss';
 
 const Postbar = () => {
+  const dispatch = useAppDispatch();
+  const { isOpen } = useAppSelector((state) => ({ ...state.postsMenu }));
+
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  const handleClick = (e: React.MouseEvent<HTMLLIElement> | undefined) => {
+    e?.stopPropagation();
+
+    if (!isOpen && screenSize <= 768) {
+      dispatch(onOpen());
+    }
+  };
+
+  const handleResize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='postbar'>
       <div className='postbar__container'>
@@ -11,21 +37,21 @@ const Postbar = () => {
           <div className='postbar__wrapper--breadcrumbs'>
             <ul className='postbar__lists'>
               <li className='postbar__lists--item'>
-                Home
-                {/* <Link to='/' className='postbar__lists--item'>
-                </Link> */}
+                <Link to='/' className='postbar__lists--item'>
+                  Home
+                </Link>
               </li>
               <span>|</span>
               <li className='postbar__lists--item'>
-                Posts
-                {/* <Link to='/posts' className='postbar__lists--item'>
-                </Link> */}
+                <Link to='/posts' className='postbar__lists--item'>
+                  Posts
+                </Link>
               </li>
               <span>|</span>
-              <li className='postbar__lists--item'>
-                Left sidebar
-                {/* <Link to='#leftSidebar' className='postbar__lists--item'>
-                </Link> */}
+              <li className='postbar__lists--item' onClick={handleClick}>
+                <Link to='#' className='postbar__lists--item'>
+                  Left sidebar
+                </Link>
               </li>
             </ul>
           </div>
