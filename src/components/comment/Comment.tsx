f404a0879971e7d3b9da8806517ea6ab55720750
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-01-27 17:42:20 +0100

    feat: refactor SCSS files to use module system and enhance responsive design across components

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -13,7 +13,46 @@ const Comment = () => {
 
   return (
     <div className='comment'>
-      <h4 className='comment__heading'>{commentHeading}</h4>
+      <div className='comment__container'>
+        <h4 className='comment__heading'>{commentHeading}</h4>
+        <figure className='comment__user'>
+          <img
+            src='/user-1.jpeg'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+          <img
+            src='/user-2.jpeg'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+          <img
+            src='/user-3.jpeg'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+          <img
+            src='/user-4.webp'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+          <img
+            src='/user-5.webp'
+            width={50}
+            height={50}
+            alt='avatar'
+            className='comment__user--img'
+          />
+        </figure>
+      </div>
       {comments.map((comment) => {
         return <CommentCard key={comment.id} {...comment} />;
       })}
