22fc437956952e15498a054fb52f089692d384ea
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-02-20 15:02:54 +0100

    feat: enhance UserMenu component; update styles for login and register links, add icons and improve layout

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -33,10 +33,38 @@ const UserMenu = () => {
         ) : (
           <div className='userMenu__box'>
             <Link to='/login' className='userMenu__box--login'>
-              Login
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9'
+                />
+              </svg>
+              <span>Login</span>
             </Link>
             <Link to='/register' className='userMenu__box--register'>
-              Register
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+                />
+              </svg>
+              <span>Register</span>
             </Link>
           </div>
         )}
