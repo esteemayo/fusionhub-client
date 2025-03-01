b85df757980399e40e4781c2b0f6df30ee98feb4
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-03-01 11:58:39 +0100

    feat: enhance responsiveness of AboutHero and ContactHero components; update styles for better adaptability across various screen sizes

--- src/components/aboutHero/AboutHero.tsx
+++ src/components/aboutHero/AboutHero.tsx
@@ -8,13 +8,7 @@ const AboutHero = () => {
           <h1 className='about-hero__wrapper--heading'>About us</h1>
           <p className='about-hero__wrapper--paragraph'>
             Welcome to our blog! Here, we share insightful articles, tips, and
-            stories on a variety of topics that matter to you. Our team of
-            dedicated writers and experts are passionate about delivering
-            high-quality content that informs, inspires, and engages our
-            readers. Whether you're looking for the latest trends, in-depth
-            analyses, or just some light reading, you'll find it all here. Thank
-            you for visiting, and we hope you enjoy exploring our blog as much
-            as we enjoy creating it for you.
+            stories on a variety of topics that matter to you.
           </p>
         </div>
       </div>
