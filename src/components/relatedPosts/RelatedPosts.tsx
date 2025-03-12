b3d85ae0ff85ebdf0dd8545e81d588aeecd3a5d8
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-03-12 16:45:51 +0100

    feat: add RelatedSkeleton component for loading state; implement responsive styling for improved user experience

--- src/components/relatedPosts/RelatedPosts.tsx
+++ src/components/relatedPosts/RelatedPosts.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from 'react';
+
 import RelatedPost from '../relatedPost/RelatedPost';
+import RelatedSkeleton from '../relatedSkeleton/RelatedSkeleton';
 
 import { relatedPosts } from '../../data';
 
 import './RelatedPosts.scss';
 
 const RelatedPosts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => setIsLoading(false), 5000);
+  // }, []);
+
   return (
     <section className='related-posts'>
       <div className='related-posts__container'>
         <h5 className='related-posts__container--heading'>Related posts</h5>
         <div className='related-posts__container--wrapper'>
-          {relatedPosts.slice(0, 4).map((post) => {
-            return <RelatedPost key={post.id} {...post} />;
-          })}
+          {isLoading
+            ? Array.from(Array(4)).map((_, index) => {
+                return <RelatedSkeleton key={index} />;
+              })
+            : relatedPosts.slice(0, 4).map((post) => {
+                return <RelatedPost key={post.id} {...post} />;
+              })}
         </div>
       </div>
     </section>
---
+++ src/components/relatedSkeleton/RelatedSkeleton.tsx
@@ -0,0 +1,40 @@
+import Skeleton from 'react-loading-skeleton';
+
+import './RelatedSkeleton.scss';
+
+const RelatedSkeleton = () => {
+  return (
+    <article className='related-skeleton'>
+      <div className='related-skeleton__container'>
+        <Skeleton
+          width={250}
+          height={150}
+          className='related-skeleton__container--img'
+        />
+        <div className='related-skeleton__footer'>
+          <Skeleton
+            width={220}
+            borderRadius={'0.5rem'}
+            className='related-skeleton__footer--link'
+          />
+          <div className='related-skeleton__wrap'>
+            <Skeleton
+              width={100}
+              height={15}
+              borderRadius={'0.5rem'}
+              className='related-skeleton__wrap--category'
+            />
+            <Skeleton
+              width={120}
+              height={15}
+              borderRadius={'0.5rem'}
+              className='related-skeleton__wrap--time'
+            />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default RelatedSkeleton;
