55cbaa9472387b5ca7e07bb7ebf673d84494f3de
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-03-17 23:49:03 +0100

    feat: enhance PostListSkeleton component with responsive widths and heights for skeleton elements

--- src/components/postListSkeleton/PostListSkeleton.tsx
+++ src/components/postListSkeleton/PostListSkeleton.tsx
@@ -6,16 +6,36 @@ const PostListSkeleton = () => {
   return (
     <article className='post-list-skeleton'>
       <div className='post-list-skeleton__wrapper'>
-        <Skeleton className='post-list-skeleton__wrapper--img' />
+        <Skeleton
+          width={'100%'}
+          height={'100%'}
+          className='post-list-skeleton__wrapper--img'
+        />
       </div>
       <div className='post-list-skeleton__footer'>
         <div className='post-list-skeleton__box'>
-          <Skeleton className='post-list-skeleton__box--title' />
-          <Skeleton className='post-list-skeleton__box--desc' />
+          <Skeleton
+            width={'100%'}
+            height={20}
+            className='post-list-skeleton__box--title'
+          />
+          <Skeleton width={'100%'} className='post-list-skeleton__box--desc' />
+          <Skeleton
+            width={'85%'}
+            className='post-list-skeleton__box--subdesc'
+          />
         </div>
         <div className='post-list-skeleton__container'>
-          <Skeleton className='post-list-skeleton__container--category' />
-          <Skeleton className='post-list-skeleton__container--time' />
+          <Skeleton
+            width={'60%'}
+            height={15}
+            className='post-list-skeleton__container--category'
+          />
+          <Skeleton
+            width={'65%'}
+            height={15}
+            className='post-list-skeleton__container--time'
+          />
         </div>
       </div>
     </article>
