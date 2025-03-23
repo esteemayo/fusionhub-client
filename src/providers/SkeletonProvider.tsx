ddefd729dae001addd494ddc79674a57c48c9683
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-03-23 19:01:16 +0100

    feat: enhance responsive design for Card and CardSkeleton components; adjust widths and media queries for improved layout adaptability

--- src/providers/SkeletonProvider.tsx
+++ src/providers/SkeletonProvider.tsx
@@ -4,7 +4,12 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const SkeletonProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SkeletonTheme baseColor='#152233' highlightColor='#273547' duration={1}>
+    <SkeletonTheme
+      baseColor='#152233'
+      highlightColor='#273547'
+      duration={1}
+      borderRadius={'0.5rem'}
+    >
       {children}
     </SkeletonTheme>
   );
