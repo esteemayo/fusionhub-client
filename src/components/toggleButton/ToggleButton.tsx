09ce91d7a7271a170108e2889ff1798bb89a8a8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-03-24 14:52:17 +0100

    feat: enhance responsive design for Header, Card, and ToggleButton components; adjust media queries and layout properties for improved adaptability across various screen sizes

--- src/components/toggleButton/ToggleButton.tsx
+++ src/components/toggleButton/ToggleButton.tsx
@@ -34,16 +34,14 @@ const ToggleButton = ({ type, label, isOpen, onClick }: ToggleButtonProps) => {
       ) : (
         <svg
           xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='size-6'
+          viewBox='0 0 20 20'
+          fill='currentColor'
+          className='size-5'
         >
           <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5'
+            fillRule='evenodd'
+            d='M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75Zm7 10.5a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1-.75-.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Z'
+            clipRule='evenodd'
           />
         </svg>
       )}
