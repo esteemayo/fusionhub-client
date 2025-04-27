3eb6d5ee7d4975af3d05f3b2d9d7d728360002a1
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-04-27 20:42:27 +0100

    feat: add additional post service functions for comments, category counts, tags, and post interactions

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -21,6 +21,14 @@ export const getLikedPosts = () => http.get(`${apiEndpoint}/liked-posts`);
 
 export const getDislikedPosts = () => http.get(`${apiEndpoint}/disliked-posts`);
 
+export const getPostComentUsers = (postId: string) =>
+  http.get(`${apiEndpoint}/comments/users/${postId}`);
+
+export const getCountByCategory = () =>
+  http.get(`${apiEndpoint}/count-by-category`);
+
+export const getTags = () => http.get(`${apiEndpoint}/tags`);
+
 export const getPost = (slug: string) =>
   http.get(`${apiEndpoint}/${slug}/details`);
 
@@ -29,3 +37,15 @@ export const createPost = <T extends object>(data: T) =>
 
 export const updatePost = <T extends object>(post: T, postId: string) =>
   http.patch(`${apiEndpoint}/${postId}`, post);
+
+export const featurePost = (postId: string) =>
+  http.patch(`${apiEndpoint}/${postId}/feature-post`);
+
+export const likePost = (postId: string) =>
+  http.patch(`${apiEndpoint}/${postId}/like`);
+
+export const dislikePost = (postId: string) =>
+  http.patch(`${apiEndpoint}/${postId}/dislike`);
+
+export const deletePost = (postId: string) =>
+  http.delete(`${apiEndpoint}/${postId}`);
