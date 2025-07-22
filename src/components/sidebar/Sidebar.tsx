7c883b37236474dd3e016a0a6872eaf956b07fed
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-07-22 14:26:20 +0100

    feat: enhance responsive styles and improve layout across multiple components

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -84,27 +84,29 @@ const Sidebar = () => {
                     {currentUser.details.name}
                   </span>
                 </NavLink>
-                <button
-                  type='button'
-                  className='sidebar__logout-btn'
-                  onClick={handleLogout}
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='size-6'
+                <div className='sidebar__logout'>
+                  <button
+                    type='button'
+                    className='sidebar__logout--btn'
+                    onClick={handleLogout}
                   >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15'
-                    />
-                  </svg>
-                  <span>{btnLabel}</span>
-                </button>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth={1.5}
+                      stroke='currentColor'
+                      className='size-6'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15'
+                      />
+                    </svg>
+                    <span>{btnLabel}</span>
+                  </button>
+                </div>
               </div>
             )}
             {!currentUser && (
