fa4c0079b8ed896061aa2ab2d352a5b03f99a05c
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-08-08 14:41:52 +0100

    feat: enhance responsive styles and refine styles for better visibility

--- src/routes/subscriptionConfirmation/SubscriptionConfirmation.tsx
+++ src/routes/subscriptionConfirmation/SubscriptionConfirmation.tsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
-import Lottie from 'lottie-react';
 import { useQuery } from '@tanstack/react-query';
-
-import { useQueryParams } from '../../utils';
-import { confirmSubscription } from '../../services/newsletterService';
+import { useMemo } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import Lottie from 'lottie-react';
 
 import errorAnimation from '../../animations/errorAnimation.json';
 import successAnimation from '../../animations/successAnimation.json';
+
+import { confirmSubscription } from '../../services/newsletterService';
 
 import './SubscriptionConfirmation.scss';
 
@@ -16,12 +16,10 @@ const subscriptionConfirmation = async (token: string, email: string) => {
 };
 
 const SubscriptionConfirmation = () => {
-  const query = useQueryParams();
+  const [searchParams] = useSearchParams();
 
-  const token = query.get('token');
-  const email = query.get('email');
-
-  console.log({ token, email });
+  const token = searchParams.get('token');
+  const email = searchParams.get('email');
 
   const { isPending, isSuccess, isError, error, data } = useQuery({
     queryKey: ['confirmSubscription'],
@@ -29,62 +27,68 @@ const SubscriptionConfirmation = () => {
     enabled: !!email && !!token,
   });
 
-  console.log(isPending);
-  console.log('error: ', error);
-  console.log('data: ', data);
+  const loadingClasses = useMemo(() => {
+    return isPending
+      ? 'subscription-confirmation__loading show'
+      : 'subscription-confirmation__loading hide';
+  }, [isPending]);
+
+  const successClasses = useMemo(() => {
+    return isSuccess && data
+      ? 'subscription-confirmation__success show'
+      : 'subscription-confirmation__success hide';
+  }, [data, isSuccess]);
+
+  const errorClasses = useMemo(() => {
+    return isError && error
+      ? 'subscription-confirmation__error show'
+      : 'subscription-confirmation__error hide';
+  }, [error, isError]);
 
   return (
     <section className='subscription-confirmation'>
       <div className='subscription-confirmation__container'>
-        {isPending && (
-          <p className='subscription-confirmation__loading'>
-            Processing your request...
+        <p className={loadingClasses}>Processing your request...</p>
+        <div className={successClasses}>
+          <div className='subscription-confirmation__success--wrapper'>
+            <h1 className='subscription-confirmation__success--title'>
+              🎉 You're Subscribed!
+            </h1>
+            <Lottie
+              animationData={successAnimation}
+              loop={true}
+              className='subscription-confirmation__success--animation'
+            />
+          </div>
+          <p className='subscription-confirmation__success--message'>
+            Thank you for confirming your subscription to our newsletter.
           </p>
-        )}
-        {isSuccess && data && (
-          <div className='subscription-confirmation__success'>
-            <div className='subscription-confirmation__success--wrapper'>
-              <h1 className='subscription-confirmation__success--title'>
-                🎉 You're Subscribed!
-              </h1>
-              <Lottie
-                animationData={successAnimation}
-                loop={true}
-                className='subscription-confirmation__success--animation'
-              />
-            </div>
-            <p className='subscription-confirmation__success--message'>
-              Thank you for confirming your subscription to our newsletter.
-            </p>
-            <p className='subscription-confirmation__success--message'>
-              You'll now receive updates, tips, and exclusive content right in
-              your inbox.
-            </p>
-            <Link to='/' className='subscription-confirmation__success--btn'>
-              Back to Home
-            </Link>
+          <p className='subscription-confirmation__success--message'>
+            You'll now receive updates, tips, and exclusive content right in
+            your inbox.
+          </p>
+          <Link to='/' className='subscription-confirmation__success--btn'>
+            Back to Home
+          </Link>
+        </div>
+        <div className={errorClasses}>
+          <div className='subscription-confirmation__error--wrapper'>
+            <h1 className='subscription-confirmation__error--title'>
+              Subscription failed
+            </h1>
+            <Lottie
+              animationData={errorAnimation}
+              loop={true}
+              className='subscription-confirmation__error--animation'
+            />
           </div>
-        )}
-        {isError && error && (
-          <div className='subscription-confirmation__error'>
-            <div className='subscription-confirmation__error--wrapper'>
-              <h1 className='subscription-confirmation__error--title'>
-                Subscription failed
-              </h1>
-              <Lottie
-                animationData={errorAnimation}
-                loop={true}
-                className='subscription-confirmation__error--animation'
-              />
-            </div>
-            <p className='subscription-confirmation__error--message'>
-              The subscribe link may be invalid or expired.
-            </p>
-            <Link to='/' className='subscription-confirmation__error--btn'>
-              Back to Home
-            </Link>
-          </div>
-        )}
+          <p className='subscription-confirmation__error--message'>
+            The subscribe link may be invalid or expired.
+          </p>
+          <Link to='/' className='subscription-confirmation__error--btn'>
+            Back to Home
+          </Link>
+        </div>
       </div>
     </section>
   );
--- src/routes/unsubscribeConfirmation/UnsubscribeConfirmation.tsx
+++ src/routes/unsubscribeConfirmation/UnsubscribeConfirmation.tsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
-import Lottie from 'lottie-react';
 import { useQuery } from '@tanstack/react-query';
-
-import { useQueryParams } from '../../utils';
-import { confirmUnSubscription } from '../../services/newsletterService';
+import { useMemo } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import Lottie from 'lottie-react';
 
 import errorAnimation from '../../animations/errorAnimation.json';
 import successAnimation from '../../animations/successAnimation.json';
+
+import { confirmUnSubscription } from '../../services/newsletterService';
 
 import './UnsubscribeConfirmation.scss';
 
@@ -16,12 +16,10 @@ const unsubscriptionConfirmation = async (token: string, email: string) => {
 };
 
 const UnsubscribeConfirmation = () => {
-  const query = useQueryParams();
+  const [searchParams] = useSearchParams();
 
-  const token = query.get('token');
-  const email = query.get('email');
-
-  // console.log({ token, email });
+  const token = searchParams.get('token');
+  const email = searchParams.get('email');
 
   const { isPending, isSuccess, isError, error, data } = useQuery({
     queryKey: ['confirmUnsubscription'],
@@ -29,60 +27,65 @@ const UnsubscribeConfirmation = () => {
     enabled: !!email && !!token,
   });
 
-  // console.log(isPending);
-  // console.log(error);
-  // console.log(data);
+  const loadingClasses = useMemo(() => {
+    return isPending
+      ? 'unsubscribe-confirmation__loading show'
+      : 'unsubscribe-confirmation__loading hide';
+  }, [isPending]);
+
+  const successClasses = useMemo(() => {
+    return isSuccess && data
+      ? 'unsubscribe-confirmation__success show'
+      : 'unsubscribe-confirmation__success hide';
+  }, [data, isSuccess]);
+
+  const errorClasses = useMemo(() => {
+    return isError && error
+      ? 'unsubscribe-confirmation__error show'
+      : 'unsubscribe-confirmation__error hide';
+  }, [error, isError]);
 
   return (
     <section className='unsubscribe-confirmation'>
       <div className='unsubscribe-confirmation__container'>
-        {isPending && (
-          <p className='unsubscribe-confirmation__loading'>
-            Processing your request...
+        <p className={loadingClasses}>Processing your request...</p>
+        <div className={successClasses}>
+          <div className='unsubscribe-confirmation__success--wrapper'>
+            <h1 className='unsubscribe-confirmation__success--title'>
+              You've been unsubscribed
+            </h1>
+            <Lottie
+              animationData={successAnimation}
+              loop={true}
+              className='unsubscribe-confirmation__success--animation'
+            />
+          </div>
+          <p className='unsubscribe-confirmation__success--message'>
+            We're sorry to see you go. You will no longer receive our
+            newsletters.
           </p>
-        )}
-        {isSuccess && data && (
-          <div className='unsubscribe-confirmation__success'>
-            <div className='unsubscribe-confirmation__success--wrapper'>
-              <h1 className='unsubscribe-confirmation__success--title'>
-                You've been unsubscribed
-              </h1>
-              <Lottie
-                animationData={successAnimation}
-                loop={true}
-                style={{ width: '5rem', height: '5rem' }}
-                className=''
-              />
-            </div>
-            <p className='unsubscribe-confirmation__success--message'>
-              We're sorry to see you go. You will no longer receive our
-              newsletters.
-            </p>
-            <Link to='/' className='unsubscribe-confirmation__success--btn'>
-              Back to Home
-            </Link>
+          <Link to='/' className='unsubscribe-confirmation__success--btn'>
+            Back to Home
+          </Link>
+        </div>
+        <div className={errorClasses}>
+          <div className='unsubscribe-confirmation__error--wrapper'>
+            <h1 className='unsubscribe-confirmation__error--title'>
+              Unsubscribe failed
+            </h1>
+            <Lottie
+              animationData={errorAnimation}
+              loop={true}
+              className='unsubscribe-confirmation__error--animation'
+            />
           </div>
-        )}
-        {isError && error && (
-          <div className='unsubscribe-confirmation__error'>
-            <div className='unsubscribe-confirmation__success--wrapper'>
-              <h1 className='unsubscribe-confirmation__error--title'>
-                Unsubscribe failed
-              </h1>
-              <Lottie
-                animationData={errorAnimation}
-                loop={true}
-                style={{ width: '5rem', height: '5rem' }}
-              />
-            </div>
-            <p className='unsubscribe-confirmation__error--message'>
-              The unsubscribe link may be invalid or expired.
-            </p>
-            <Link to='/' className='unsubscribe-confirmation__error--btn'>
-              Back to Home
-            </Link>
-          </div>
-        )}
+          <p className='unsubscribe-confirmation__error--message'>
+            The unsubscribe link may be invalid or expired.
+          </p>
+          <Link to='/' className='unsubscribe-confirmation__error--btn'>
+            Back to Home
+          </Link>
+        </div>
       </div>
     </section>
   );
